1a5a04dae7b8b0dcc56c755a00b7a296e62c44e8
jhesski <jhesski@gmail.com>
2013-11-07 20:48:32 -0500

    classy game
    arrow change color click starts game

--- Main.js
+++ Main.js
@@ -2,28 +2,44 @@
  * Created by hessj on 11/3/13.
  */
 enchant();
-var preLoadList = new Array();
+var arrowsPerScreen = 9
+var fps = 60;
+var songStart = false;
 
+var Console = enchant.Class.create(enchant.Scene,{
+    initialize: function(bpm){
+        var game = enchant.Core.instance;
+        game.preload(preLoadFiles());
+        game.scale = 2;
+        game.fps = fps;
+        game.bpm = 85;
+    }
+});
 window.onload = function() {
-    preLoadFiles();
     var game = new Core(694, 366);
-    var fps = 30;
-    game.preload(preLoadList);
-    game.scale = 2;
-    game.fps = fps;
-
-    var arrowsPerScreen = 9;
-    var bpm = 128;
+    var test = new Console()
+    var bpm = 85;
     var fpb = Math.round(game.fps/(bpm/60));
     var startDelay = 2 * fps;
 
+    game.rootScene.addEventListener('touchstart', function(e) {
+        var ea = new enchant.Event("startSong");
+        game.rootScene.dispatchEvent(ea);
+    });
+
     game.onload = function(){
-        var beatarrow = new beatArrow();
         var arrows = new ArrowBase(60,300);
         var lastFrame;
-        var startText = new beginText();
 
-        game.rootScene.addChild(beatarrow);
+        var songTitle = new Label();
+        songTitle.font = "13px Helvetica";
+
+        songTitle.color = "#f8b800";
+        songTitle.x = 280;
+        songTitle.y = 20;
+
+        game.rootScene.addChild(songTitle);
+
         game.rootScene.backgroundColor = '#080808';
         game.addEventListener("enterframe", function(){
             if(game.frame > 1){
@@ -31,40 +47,25 @@ window.onload = function() {
                 var nextBeat = game.frame % fpb;
                 if(nextBeat < 8 ){
                     this.beatHit = true;
+
                 }else{
+
                     this.beatHit = false;
                 }
                 game.rootScene.dispatchEvent(e);
                 lastFrame = game.frame;
             };
-        });
-
-        beatarrow.addEventListener("enterframe", function(){
-            if(Math.round(this.y) >= 350 || (game.frame > startDelay && game.frame < startDelay + 2)){
-                this.opacity = 1;
+            // BEAT......
+            if(game.frame > startDelay && nextBeat == 0){};
+            if(nextBeat == 0 && songStart){
                 var arrowPose = Math.floor((Math.random()*4)+1);
-                if(arrowPose == 1){
-                    this.rotation = -90;
-                    this.x=56;
-                };
-                if(arrowPose == 2){
-                    this.rotation = -180;
-                    this.x=96;
-                };
-                if(arrowPose == 3){
-                    this.rotation = 0;
-                    this.x=136;
-                };
-                if(arrowPose == 4){
-                    this.rotation = 90;
-                    this.x=176;
-                };
-                this.y = 10;
-                this.tl.moveTo(this.x,350,fpb*arrowsPerScreen);
+                var beatarrow = new beatArrow(arrowPose);
             };
-            //label.text = " BPM: "+ bpm;//"Arrow y: "+ Math.round(this.y);
         });
-
+        game.rootScene.addEventListener('startSong',function(){
+            new beginText();
+            songStart = true;
+        });
     };
     game.start();
 
@@ -100,24 +101,82 @@ window.onload = function() {
     })
 
     var beatArrow = enchant.Class.create(enchant.Sprite,{
-        initialize: function(){
-            enchant.Sprite.call(this,36 ,36);
-            this.image =       game.assets["img/ArrowGlow.png"];
-            this.frame = 1;
-            this.opacity=0;
+        initialize: function(arrowPose){
+            enchant.Sprite.call(this,35 ,35);
+            this.arrow = new Arrow02();
+            if(arrowPose == 1){
+                this.arrow.rotation = -90;
+                this.arrow.x=56;
+            };
+            if(arrowPose == 2){
+                this.arrow.rotation = -180;
+                this.arrow.x=96;
+            };
+            if(arrowPose == 3){
+                this.arrow.rotation = 0;
+                this.arrow.x=136;
+            };
+            if(arrowPose == 4){
+                this.arrow.rotation = 90;
+                this.arrow.x=176;
+            };
+            this.arrow.opacity=0;
+            game.rootScene.addChild(this.arrow);
+            this.arrow.addEventListener("enterframe",function(){
+                if(this.age == 1){
+                    this.opacity = 0;
+                    this.tl.setFrameBased();
+                    this.fpb = Math.round(game.fps/(bpm/60)*arrowsPerScreen)
+                    this.tl.moveTo(this.x,350,this.fpb).and().fadeIn(30);
+                };
+                if(Math.round(this.y) == 350){
+                  game.rootScene.removeChild(this);
+                };
+            });
         }
     });
+
+    //--------- Arrow 2 ----------------\\
+    var Arrow02 = enchant.Class.create(enchant.Sprite,{
+        initialize: function(arrowPose){
+            this.startFrame = 1;
+            enchant.Sprite.call(this,35 ,35);
+            this.image =       game.assets["img/ArrowGlow.png"];
+            this.fader = new Sprite(35,35);
+            this.fader.image=  game.assets["img/ArrowGlow.png"];
+            this.fader.frame = 1;
+            this.tl.setFrameBased();
+            this.fader.tl.setFrameBased();
+            //game.rootScene.addChild(this);
+            //game.rootScene.addChild(this.fader);
+            this.addEventListener("enterframe",function(){
+                if(this.age % 12 == 0){
+                    this.frame = this.startFrame;
+                    this.fader.frame = this.startFrame +1;
+                    this.tl.fadeOut(6).fadeIn(6);
+                    this.fader.tl.fadeIn(6).fadeOut(6);
+                    this.startFrame += 1;
+                    if(this.startFrame > 5){
+                        this.startFrame =0;
+                    };
+                }
+            });
+
+        }
+    });
+
+    // -------- Start Text -------- \\
     var beginText = enchant.Class.create(enchant.Sprite,{
         initialize: function(){
             enchant.Sprite.call(this,83 ,41);
             this.image = game.assets["img/battleText.png"];
             this.frame = 0;
-            this.x = 80;
-            this.y = 150;
+            this.x = 92;
+            this.y = 150; //@TODO
             this.opacity = 0;
             game.rootScene.addChild(this);
             this.addEventListener("enterframe",function(){
-                if(game.frame == 0){
+                if(this.age < 60){
                     this.tl.fadeIn(30).fadeOut(30).then(function(){this.opacity=0;});
                 };
                 if(this.age == 60){
@@ -131,6 +190,7 @@ window.onload = function() {
             });
         }
     });
+
     var arrow01 = enchant.Class.create(enchant.Sprite,{
         initialize: function(x, y){
             enchant.Sprite.call(this,27 ,27);
@@ -147,8 +207,9 @@ window.onload = function() {
 
 
 function preLoadFiles(){
-    preLoadList.push("img/ArrowGlow.png");
-    preLoadList.push("img/Arrow.png");
-    preLoadList.push("img/battleText.png");
-
+    var fileList = new Array();
+    fileList.push("img/ArrowGlow.png");
+    fileList.push("img/Arrow.png");
+    fileList.push("img/battleText.png");
+    return(fileList);
 };
