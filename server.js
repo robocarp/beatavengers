dcad68417b0a3fc5d60c68403792b0695ca938a5
lostmindio <jonathan.m.barton@gmail.com>
2013-11-10 01:40:33 -0500

    awesome

--- server.js
+++ server.js
@@ -12,6 +12,37 @@ var express = require("express")
     , http = require("http").createServer(app)
     , io = require("socket.io").listen(http);
 
+function GameLoop(){
+    this._interval = 1000;
+    this._timer = null;
+}
+GameLoop.prototype = {
+    init: function(){
+        this._timer = setInterval(this._tick, this._interval);
+    },
+    _tick: function(){
+        io.sockets.in('test').emit('news', {'server_time':Date.now(),'room_name':'test'});
+        console.log('ticking');
+    }
+
+}
+
+var gloop = new GameLoop();
+gloop.init();
+
+io.sockets.on('connection', function (socket) {
+    //socket.emit('news', { hello: 'world' });
+    //console.log('got here');
+    socket.join('test');
+
+    /*
+     socket.on('my other event', function (data) {
+     console.log(data);
+     });
+     */
+
+});
+
 app.get('/hello.txt', function(req, res){
     var body = 'Hello World';
     res.setHeader('Content-Type', 'text/plain');
