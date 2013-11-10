dc1d069beb9c0a81b030bb09ff228491dcebc7c2
lostmindio <jonathan.m.barton@gmail.com>
2013-11-10 00:52:55 -0500

    websockets test

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
-var express = require("express");
-var app = express();
-app.use(express.logger());
-
-app.get('/', function(request, response) {
-    response.send('Hello World!');
-});
-
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-    console.log("Listening on " + port);
+
+var server = http.createServer(app);
+server.listen(port);
+
+var wss = new WebSocketServer({server: server});
+console.log('websockt server created');
+wss.on('connection', function(ws) {
+    var id = setInterval(function() {
+        ws.send(JSON.stringify(new Date()), function() {  });
+    }, 1000);
+
+    console.log('websocket connection open');
+
+    ws.on('close', function() {
+        console.log('websocket connection close');
+        clearInterval(id);
+    });
 });
