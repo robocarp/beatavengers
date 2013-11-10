e95237868fd63c96b61291e5202053f1e1aa6c04
lostmindio <jonathan.m.barton@gmail.com>
2013-11-10 01:11:21 -0500

    awesome

--- server.js
+++ server.js
@@ -1,19 +1,64 @@
-var port = process.env.PORT || 5000;
+/*
+ Module dependencies:
 
-var server = http.createServer(app);
-server.listen(port);
+ - Express
+ - Http (to run Express)
 
-var wss = new WebSocketServer({server: server});
-console.log('websockt server created');
-wss.on('connection', function(ws) {
-    var id = setInterval(function() {
-        ws.send(JSON.stringify(new Date()), function() {  });
-    }, 1000);
+ It is a common practice to name the variables after the module name.
+ Ex: http is the "http" module, express is the "express" module, etc.
+ */
+var express = require("express")
+    , app = express()
+    , http = require("http").createServer(app)
+    , io = require("socket.io").listen(http);
 
-    console.log('websocket connection open');
 
-    ws.on('close', function() {
-        console.log('websocket connection close');
-        clearInterval(id);
+/* Server config */
+//app.set("ipaddr", "127.0.0.1");
+app.set("port", 8080);
+app.use(express.bodyParser()); //Tells server to support JSON, urlencoded, and multipart requests
+
+//Handle route "GET /", as in "http://localhost:8080/"
+
+app.get("/", function(request, response) {
+    response.send("Server is up and running");
+});
+
+
+function GameLoop(){
+    this._interval = 1000;
+    this._timer = null;
+}
+GameLoop.prototype = {
+    init: function(){
+        this._timer = setInterval(this._tick, this._interval);
+    },
+    _tick: function(){
+        io.sockets.in('test').emit('news', {'server_time':Date.now(),'room_name':'test'});
+        console.log('ticking');
+    }
+
+}
+
+var gloop = new GameLoop();
+gloop.init();
+
+
+io.sockets.on('connection', function (socket) {
+    //socket.emit('news', { hello: 'world' });
+    //console.log('got here');
+    socket.join('test');
+
+    /*
+    socket.on('my other event', function (data) {
+        console.log(data);
     });
+    */
+
+});
+
+
+
+http.listen(app.get("port"), app.get("ipaddr"), function() {
+    console.log("Server up and running. Go to http://" + app.get("ipaddr") + ":" + app.get("port"));
 });
