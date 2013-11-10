876dda70ec46c9069ab7acedabf98e225f4f224f
lostmindio <jonathan.m.barton@gmail.com>
2013-11-10 00:38:09 -0500

    changed port on test

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get('/', function(request, response) {
     response.send('Hello World!');
 });
 
-var port = 8080;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
     console.log("Listening on " + port);
 });
