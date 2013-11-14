0590c23a77c6baf8454fe55314e3b860e281c89b
jhesski <jhesski@gmail.com>
2013-11-14 03:00:20 -0500

    Light it Up like Christmas
    Krampus

---
+++ game_classes.js
@@ -0,0 +1,45 @@
+/**
+ * Created by hessj on 11/11/13.
+ */
+
+var init_Game = enchant.Class.create(enchant.Scene,{
+    initialize: function(bpm){
+        var game = new Core(1388, 732);
+        game.rootScene.backgroundColor = "#f00";
+        game.preload(preLoadFiles());
+        game.fps = fps;
+        game.bpm = 135;
+    }
+});
+
+//----------- Button Event ------------------\\
+var Local_Player_Input = function (ConsoleClass) {
+    // BUTTON UP
+    var game = enchant.Core.instance;
+    var test = game.input.up;
+    if (game.input.up) {
+        ConsoleClass.arrowBase.ArrowUp.opacity = 1;
+    }
+    if (!game.input.up) {
+        ConsoleClass.arrowBase.ArrowUp.tl.fadeOut(3);
+    }
+    if (game.input.down) {
+        ConsoleClass.arrowBase.ArrowDown.opacity = 1;
+    }
+    if (!game.input.down) {
+        ConsoleClass.arrowBase.ArrowDown.tl.fadeOut(3);
+    }
+    if (game.input.left) {
+        ConsoleClass.arrowBase.ArrowLeft.opacity = 1;
+    }
+    if (!game.input.left) {
+        ConsoleClass.arrowBase.ArrowLeft.tl.fadeOut(3);
+    }
+    if (game.input.right) {
+        ConsoleClass.arrowBase.ArrowRight.opacity = 1;
+    }
+    if (!game.input.right) {
+        ConsoleClass.arrowBase.ArrowRight.tl.fadeOut(3);
+    }
+    //});
+};
